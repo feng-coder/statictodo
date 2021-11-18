39fc90e4e71295a91272fffc2a3465b952e3bb41
feng-coder <83321185+feng-coder@users.noreply.github.com>
2021-11-17 23:02:05 -0500

    Add files via upload

---
+++ precache-manifest.5eb7deb15c1fcef563b4ebf458407a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1d9a595d64fe1306003",
+    "url": "/static/css/main.2e372b6d.chunk.css"
+  },
+  {
+    "revision": "b1d9a595d64fe1306003",
+    "url": "/static/js/main.b1d9a595.chunk.js"
+  },
+  {
+    "revision": "6a02d334723e77f6e9cc",
+    "url": "/static/css/1.f3ca46c4.chunk.css"
+  },
+  {
+    "revision": "6a02d334723e77f6e9cc",
+    "url": "/static/js/1.6a02d334.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b8ac3a16ffcdabc830e441e075bdcd8",
+    "url": "/index.html"
+  }
+];
